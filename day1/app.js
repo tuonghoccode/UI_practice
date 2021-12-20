c8d933f96e24699c6acbf884577462ed7f73355e
tuong.lau16072002@hcmut.edu.vn <tuong.lau16072002@hcmut.edu.vn>
2021-12-20 21:11:12 +0700

    first-commit

---
+++ day1/app.js
@@ -0,0 +1,21 @@
+const sidebar_click = document.querySelectorAll(".wrapper__sidebar i");
+const header_click = document.querySelectorAll(
+  ".wrapper__content__header--right i"
+);
+
+
+function active_sidebar(){
+    sidebar_click.forEach(item=>{
+        if (item!=this) item.classList.remove("active");
+    })
+    this.classList.toggle("active");
+}
+function active_header() {
+  header_click.forEach((item) => {
+    if (item != this) item.classList.remove("active");
+  });
+  this.classList.toggle("active");
+}
+
+sidebar_click.forEach((item) => item.addEventListener("click",active_sidebar));
+header_click.forEach((item) => item.addEventListener("click", active_header));
